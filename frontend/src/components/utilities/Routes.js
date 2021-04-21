351eae2497b535739bd010a4af47860221b9f080
Will MacCarty <wmaccarty@gmail.com>
2021-04-20 23:49:06 -0400

    route to require access to some pages added

---
+++ frontend/src/components/utilities/Routes.js
@@ -0,0 +1,30 @@
+  
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+export const ProtectedRoute = ({ component: Component, path, currentUser, exact, history }) => {
+  return (
+    <Route path={path} exact={exact} render={(props) => (
+      currentUser.is_authenticated && !currentUser.is_anonymous ?
+        <Component {...props} />
+        : <Redirect to='/' />
+    )} />
+  )
+}
+
+export const AuthRoute = ({ component: Component, path, currentUser, exact, history }) => {
+
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={(props) =>
+        currentUser.is_authenticated ?
+          <Redirect to={`/users/${currentUser.id}/campaigns`} />
+          : <Component {...props} />
+      }
+    />
+  );
+};
+
+//tbr
