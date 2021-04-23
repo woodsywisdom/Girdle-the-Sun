89895a489ba59bd423f7f09766751470acc66d00
Will MacCarty <wmaccarty@gmail.com>
2021-04-22 23:26:37 -0400

    created PWP, card containing stats on the project you are currently working on

---
+++ frontend/src/components/PresentWorkingProject.js
@@ -0,0 +1,33 @@
+  
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import TagNotes from './TagNotes';
+
+
+const useStyles = makeStyles(theme => ({
+  corkboard: {
+    flexDirection: 'row',
+  }
+}));
+
+const CorkBoard = () => {
+  const classes = useStyles();
+  const pinnedTags = useSelector(state => state.ui.pinnedTags);
+
+
+  return (
+    <>
+      <Box className={classes.corkboard} display='flex'>
+        {pinnedTags.map((tag, index) => <TagNotes key={index} position={index} tag={tag} />)}
+      </Box>
+    </>
+  );
+}
+
+export default CorkBoard;
+
+//tbr
