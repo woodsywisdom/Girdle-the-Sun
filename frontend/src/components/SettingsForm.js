833e16c04b7830434e4bcd60043fb868a4989493
Will MacCarty <wmaccarty@gmail.com>
2021-04-23 23:43:36 -0400

    more setting functionality

--- frontend/src/components/SettingsForm.js
+++ frontend/src/components/SettingsForm.js
@@ -13,6 +13,70 @@ const useStyles = makeStyles({
   }
 })
 
+const Navbar = () => {
+    const dispatch = useDispatch();
+    const classes = useStyles();
+    const currentCampaign = useSelector(state => state.ui.currentCampaign);
+    const campaigns = useSelector(state => state.entities.campaigns)
+    const currentUser = useSelector(state => state.auth);
+    const [anchorEl, setAnchorEl] = useState(null);
+  
+    useEffect(() => {
+      dispatch(loadCampaigns(currentUser.id));
+    }, [dispatch, currentUser]);
+  
+    const logoClick = e => window.location = '/';
+  
+    const handleLogin = (e) => dispatch(openLogin());
+  
+    const handleLogout = (e) => dispatch(logout());
+  
+    const openCampaigns = (e) => setAnchorEl(e.currentTarget);
+  
+    const closeCampaigns = (e) => setAnchorEl(null);
+  
+    const campaignClick = (e) => window.location = `/campaigns/${e.currentTarget.value}`;
+  
+    return (
+      <>
+        <AppBar className={classes.appBar} position='fixed' >
+          <Toolbar display='flex' variant="dense" className={classes.toolbar} >
+            <Box className={classes.logoButton} onClick={logoClick} >
+              <Typography variant='button'>LoreKeeper</Typography>
+            </Box>
+            <Box>
+              {currentUser.id ? (
+                <>
+                  <Button onClick={openCampaigns} endIcon={<ExpandMoreIcon />}>
+                    {currentCampaign.title ? currentCampaign.title
+                      : <Typography variant='button'>Select a Campaign!</Typography>}
+                  </Button>
+                  <Menu
+                    anchorEl={anchorEl}
+                    keepMounted
+                    onClose={closeCampaigns}
+                    open={!!anchorEl}
+                  >
+                    {campaigns ? Object.values(campaigns).map((campaign) => (
+                      <MenuItem key={campaign.id} value={campaign.id} onClick={campaignClick} >
+                        {campaign.title}
+                      </MenuItem>
+                    )) : null}
+                  </Menu>
+                </>) : null}
+            </Box>
+            <Box>
+              {currentUser.is_authenticated ?
+                <Button variant='outlined' onClick={handleLogout}>Logout</Button>
+                : <Button variant='outlined' onClick={handleLogin}>Login</Button>
+              }
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </>
+    );
+  };
+
 
 const TagEditForm = () => {
   const dispatch = useDispatch();
