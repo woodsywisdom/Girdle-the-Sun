ab889b39618fb5d74a5bdd31dcc69455b26d5a58
Will MacCarty <wmaccarty@gmail.com>
2021-04-23 23:08:51 -0400

    loaded styles into App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+import Navbar from './components/navbar/Navbar';
+import { setUser } from './store/auth';
+import CustomSwitch from './CustomSwitch';
+import WelcomePage from './components/welcomepage/WelcomePage';
+
+// const mahoganyTheme = createMuiTheme({
+//     "palette": {
+//         "common": {
+//             "black":"#000",
+//             "white":"#fff",
+//         },
+//         "background": {
+//             // "paper": "rgba(76, 24, 15, 1)",
+//             "paper":"rgba(67, 24, 17, 1)",
+//         },
+//         "primary": {
+//             "light":"rgba(198, 131, 111, 1)",
+//             "main":"rgba(143, 67, 43, 1)",
+//             "dark":"rgba(99, 35, 15, 1)",
+//             "contrastText":"#fff",
+//         },
+//         "secondary": {
+//             "light":"rgba(244, 205, 160, 1)",
+//             "main":"rgba(236, 176, 105, 1)",
+//             "dark":"rgba(188, 109, 17, 1)",
+//             "contrastText":"rgba(0, 0, 0, 1)",
+//         },
+//         "error": {
+//             "light":"#e57373",
+//             "main":"rgba(208, 2, 27, 1)",
+//             "dark":"#d32f2f",
+//             "contrastText":"#fff",
+//         },
+//         "text": {
+//             "primary":"rgba(255, 255, 255, 1)",
+//             "secondary":"rgba(0, 0, 0, 1)",
+//             "disabled":"rgba(248, 231, 28, 1)",
+//             "hint":"rgba(139, 87, 42, 1)",
+//         }
+//     }
+// });
+
+const darkTheme = createMuiTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            main: '#9d0a0e',
+        },
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const dispatch = useDispatch();
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const setCSRF = async () => {
+            const res = await fetch('/api/session/csrf/');
+            if (res.ok) {
+                return;
+            }
+        }
+        setCSRF();
+    }, []);
+
+    useEffect(() => {
+        const loadUser = async () => {
+            const res = await fetch('/api/session/current-user/');
+            const data = await res.json();
+            if (res.ok && !data.errors) {
+                dispatch(setUser(data.user));
+            }
+            setLoading(false);
+        }
+        loadUser();
+    }, [dispatch]);
+
+    const currentUser = useSelector(state => state.auth)
+
+    if (loading) {
+        return '...loading';
+    }
+
+    return (
+        <ThemeProvider theme={darkTheme} >
+            <CssBaseline />
+            <BrowserRouter>
+                <Navbar />
+                <CustomSwitch user={currentUser} />
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
 
 export default App;
+
+//tbr
---
+++ frontend/src/components/Help.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import { Card, CardContent, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  helpCard: {
+    width: '320px',
+    position: 'fixed',
+    left: '860px',
+  }
+});
+
+const Help = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.helpCard} >
+      <CardContent>
+        <Typography >How to use: Just start typing!  Every note automatically includes the current session's tag, but you can add existing or new tags by just adding a '#' at the beginning of its name! (Avoid puntuation like apostrophes and commas.  Hyphens are cool though)</Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Help;
