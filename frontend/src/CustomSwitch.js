0bf021b9e8819a87a9b730dc3778fdbae6c6d600
Will MacCarty <wmaccarty@gmail.com>
2021-04-23 23:16:45 -0400

    made a new protected switch for auth

---
+++ frontend/src/CustomSwitch.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import { ProtectedRoute, AuthRoute } from './components/utils/Routes';
+import WelcomePage from './components/welcomepage/WelcomePage';
+import CampaignsPage from './components/campaigns/CampaignsPage';
+import Dashboard from './components/dashboard/Dashboard';
+
+
+const CustomSwitch = ({user}) => {
+
+  return (
+    <>
+      <Switch>
+        <ProtectedRoute path='/users/:userId/campaigns' exact component={CampaignsPage} currentUser={user} />
+        <ProtectedRoute path='/campaigns/:id' exact component={Dashboard} currentUser={user} />
+        {/* <AuthRoute path='/welcome' exact component={WelcomePage} currentUser={user} /> */}
+        <AuthRoute path='/' exact component={WelcomePage} currentUser={user} />
+      </Switch>
+    </>
+  );
+}
