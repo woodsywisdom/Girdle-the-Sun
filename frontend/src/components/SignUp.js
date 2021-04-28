70d28aed9f640b74feb042d9ae9b80987177531c
Will MacCarty <wmaccarty@gmail.com>
2021-04-27 23:31:28 -0400

    new styles for SignUp

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -4,6 +4,32 @@ import { connect } from 'react-redux';
 import { submitQuestion } from '../actions/question_actions';
 
 
+const useStyles = makeStyles({
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '16px',
+  },
+  loginContainer: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '16px 0px',
+  },
+  signUpContainer: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '16px 0px',
+  },
+  inputField: {
+    margin: '8px 20px',
+  },
+  errorList: {
+    color: 'red',
+  }
+})
+
 const Form = () => {
   const [ name, setName ] = useSate('');
   const [ text, setText ] = useState('');
