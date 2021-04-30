64971d58841c5afa24074aa85008b87e021f7629
Will MacCarty <wmaccarty@gmail.com>
2021-04-29 23:55:40 -0400

    fixed styling bug in PWD by implementing a dialog rather than hidden card

--- frontend/src/components/PresentWorkingProject.js
+++ frontend/src/components/PresentWorkingProject.js
@@ -26,6 +26,32 @@ const CorkBoard = () => {
       </Box>
       <CampaignForm open={formOpen} />
       <Container className={classes.campaignsContainer}>
+      <Dialog open={tagToEdit.id} onClose={closeForm} >
+        <Card >
+          <CardContent display="flex" flexDirections="column" alignItems="center" >
+
+            <Typography className={classes.title} variant='h5'>Edit Tag - {tagToEdit.name}</Typography>
+            {/* <TextField
+              defaultValue={tagToEdit.name}
+              value={newName}
+              onChange={changeName}
+              label='New Tag Name' /> */}
+            <Select
+              defaultValue={tagToEdit.category_id}
+              value={newCategoryId}
+              onChange={changeId}
+            >
+              {/* <option value={tagToEdit.category_id}>{currentCategory.name}</option> */}
+              {categories.map(category => <option key={category.id} value={category.id}>{category.name}</option>)}
+            </Select>
+
+          </CardContent>
+          <CardActions>
+            <Button onClick={handleSave} >Save</Button>
+            <Button onClick={closeForm}>Cancel</Button>
+          </CardActions>
+        </Card>
+      </Dialog>
         <Typography className={classes.yourCampaignsHeader} variant='h4'>
           Your Campaigns
           <Button className={classes.addButton} variant='contained' onClick={handleClick}>
