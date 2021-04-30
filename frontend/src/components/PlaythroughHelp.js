35f0bc81a42f14ac7b1e9507bc6f1f9df21c841a
Will MacCarty <wmaccarty@gmail.com>
2021-04-29 23:39:14 -0400

    fleshed out instructions in playthrough help

--- frontend/src/components/PlaythroughHelp.js
+++ frontend/src/components/PlaythroughHelp.js
@@ -39,11 +39,29 @@ const Help = () => {
   const classes = useStyles();
 
   return (
-    <Card className={classes.helpCard} >
-      <CardContent>
-        <Typography >How to use: Just start typing!  Every note automatically includes the current session's tag, but you can add existing or new tags by just adding a '#' at the beginning of its name! (Avoid puntuation like apostrophes and commas.  Hyphens are cool though)</Typography>
-      </CardContent>
-    </Card>
+    <>
+        <Box className={classes.welcomeContainer}>
+        <Card className={classes.welcomeCard} variant='outlined'>
+          <CardContent >
+            <Typography variant='h2' gutterBottom align='center' >Welcome to LoreKeeper!</Typography>
+            <Typography align='center' >LoreKeeper is a dynamic notetaking app for your Dungeons and Dragons or TTRPG game.  <br /><br />
+              Intuitively create notes connected to topics (e.g. NPCs, Locations, etc.) by including hashtags in your notes, <br />
+              then easily access all notes associated with those topics.  <br /><br />
+              Sign up and see how LoreKeeper can make running your game a lower CR monster than you ever thought possible! </Typography>
+          </CardContent>
+          <Box className={classes.actionsBox} >
+            <CardActions right='24px' >
+              <Button onClick={loginOpen} variant='contained' color='primary' >Login / Sign Up</Button>
+            </CardActions>
+          </Box>
+        </Card>
+      </Box>
+        <Card className={classes.helpCard} >
+        <CardContent>
+            <Typography >How to use: Just start typing!  Every note automatically includes the current session's tag, but you can add existing or new tags by just adding a '#' at the beginning of its name! (Avoid puntuation like apostrophes and commas.  Hyphens are cool though)</Typography>
+        </CardContent>
+        </Card>
+    </>
   );
 }
 
