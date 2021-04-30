f7fcbc5051103bec8bfed99756cb9d1dfbfcb552
Will MacCarty <wmaccarty@gmail.com>
2021-04-29 23:46:04 -0400

    added actual functionality to the PWD component

--- frontend/src/components/PresentWorkingProject.js
+++ frontend/src/components/PresentWorkingProject.js
@@ -24,6 +24,45 @@ const CorkBoard = () => {
       <Box className={classes.corkboard} display='flex'>
         {pinnedTags.map((tag, index) => <TagNotes key={index} position={index} tag={tag} />)}
       </Box>
+      <CampaignForm open={formOpen} />
+      <Container className={classes.campaignsContainer}>
+        <Typography className={classes.yourCampaignsHeader} variant='h4'>
+          Your Campaigns
+          <Button className={classes.addButton} variant='contained' onClick={handleClick}>
+            <AddIcon />
+          </Button>
+
+        </Typography>
+        <Grid container spacing={3} >
+          {campaigns ? Object.values(campaigns).map(campaign => {
+            return (
+              <CampaignCard
+                key={campaign.id}
+                campaign={campaign}
+              />
+            );
+          }) : <></>}
+        </Grid>
+        {/* <List>
+            <ListItem>
+              <ListItemText>
+                <Typography variant='button'>{title}</Typography>
+              </ListItemText>
+            </ListItem>
+            <ListItem>
+              <ListItemText>
+                <Typography variant='caption'>{description}</Typography>
+              </ListItemText>
+            </ListItem>
+          </List>
+          <CardActions className={classes.cardActions}>
+            <Button onClick={handleDelete}>
+              <DeleteIcon />
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid> */}
+      </Container>
     </>
   );
 }
