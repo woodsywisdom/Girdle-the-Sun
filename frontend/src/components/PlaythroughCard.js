fd1bbf7c68d7a19d1f0550de566833061bf29de6
Will MacCarty <wmaccarty@gmail.com>
2021-04-30 23:24:25 -0400

    reworked playthrough card to be more readable

--- frontend/src/components/PlaythroughCard.js
+++ frontend/src/components/PlaythroughCard.js
@@ -23,31 +23,62 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const CampaignCard = ({campaign}) => {
+const CorkBoard = () => {
   const classes = useStyles();
-  const dispatch = useDispatch();
+  const pinnedTags = useSelector(state => state.ui.pinnedTags);
 
-  const { id, title, description } = campaign;
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    window.location = `/campaigns/${e.currentTarget.id}`;
-  }
-  const handleDelete = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    dispatch(deleteCampaign(id));
-  }
 
   return (
     <>
-      <Grid item xs={4}>
-        <Card
-          onClick={handleClick}
-          id={id}
-          className={classes.campaignCard}
-        >
-          <List>
+      <Box className={classes.corkboard} display='flex'>
+        {pinnedTags.map((tag, index) => <TagNotes key={index} position={index} tag={tag} />)}
+      </Box>
+      <CampaignForm open={formOpen} />
+      <Container className={classes.campaignsContainer}>
+      <Dialog open={tagToEdit.id} onClose={closeForm} >
+        <Card >
+          <CardContent display="flex" flexDirections="column" alignItems="center" >
+
+            <Typography className={classes.title} variant='h5'>Edit Tag - {tagToEdit.name}</Typography>
+            {/* <TextField
+              defaultValue={tagToEdit.name}
+              value={newName}
+              onChange={changeName}
+              label='New Tag Name' /> */}
+            <Select
+              defaultValue={tagToEdit.category_id}
+              value={newCategoryId}
+              onChange={changeId}
+            >
+              {/* <option value={tagToEdit.category_id}>{currentCategory.name}</option> */}
+              {categories.map(category => <option key={category.id} value={category.id}>{category.name}</option>)}
+            </Select>
+
+          </CardContent>
+          <CardActions>
+            <Button onClick={handleSave} >Save</Button>
+            <Button onClick={closeForm}>Cancel</Button>
+          </CardActions>
+        </Card>
+      </Dialog>
+        <Typography className={classes.yourCampaignsHeader} variant='h4'>
+          Your Campaigns
+          <Button className={classes.addButton} variant='contained' onClick={handleClick}>
+            <AddIcon />
+          </Button>
+
+        </Typography>
+        <Grid container spacing={3} >
+          {campaigns ? Object.values(campaigns).map(campaign => {
+            return (
+              <CampaignCard
+                key={campaign.id}
+                campaign={campaign}
+              />
+            );
+          }) : <></>}
+        </Grid>
+        {/* <List>
             <ListItem>
               <ListItemText>
                 <Typography variant='button'>{title}</Typography>
@@ -65,8 +96,8 @@ const CampaignCard = ({campaign}) => {
             </Button>
           </CardActions>
         </Card>
-      </Grid>
-
+      </Grid> */}
+      </Container>
     </>
   );
 }
